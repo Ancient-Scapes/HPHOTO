10f4859909b5d122569ae0dcfa270f34d67c17be
SugarShootingStar <ryu.8224@gmail.com>
2018-11-17 17:44:17 +0900

    init

---
+++ lib/hphoto.ex
@@ -0,0 +1,18 @@
+defmodule Hphoto do
+  @moduledoc """
+  Documentation for Hphoto.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Hphoto.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/hphoto_test.exs
@@ -0,0 +1,8 @@
+defmodule HphotoTest do
+  use ExUnit.Case
+  doctest Hphoto
+
+  test "greets the world" do
+    assert Hphoto.hello() == :world
+  end
+end
