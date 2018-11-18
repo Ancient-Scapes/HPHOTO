5d7f034fc5feacd0c51e4915b0653ca5d5f160e2
SugarShootingStar <ryu.8224@gmail.com>
2018-11-18 23:28:15 +0900

    feat:extwitterを導入して使えるように設定する所まで

--- mix.exs
+++ mix.exs
@@ -11,18 +11,17 @@ defmodule Hphoto.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
   def application do
     [
+      applications: [:logger, :extwitter],
       extra_applications: [:logger]
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:oauther, "~> 1.1"},
+      {:extwitter, "~> 0.8"}
     ]
   end
 end
