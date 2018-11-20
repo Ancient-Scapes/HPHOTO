7780cce583f8341692ac1afdb137ba59f553e3e6
SugarShootingStar <ryu.8224@gmail.com>
2018-11-20 18:09:02 +0900

    feat:自分のリストから画像ツイートの画像URLを抽出してリスト取得するところまで

--- lib/hphoto.ex
+++ lib/hphoto.ex
@@ -1,18 +1,30 @@
 defmodule Hphoto do
-  @moduledoc """
-  Documentation for Hphoto.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Hphoto.hello()
-      :world
-
-  """
-  def hello do
-    :world
+  def main do
+    fetchListTimeLine()
+    |> Enum.each(fn (t) -> IO.inspect t end)
   end
+
+  def fetchListTimeLine do
+    # TODO リストの選択
+    ExTwitter.list_timeline("list5", "_Ancient_Scapes", [count: 10])
+    |> Enum.map(&(extractionTweet(&1)))
+  end
+
+  def extractionTweet(t) do
+    if Map.has_key?(t.entities, :media) do
+      %{
+        "id" => t.id_str,
+        "name" => "@" <> t.user.screen_name <> ":" <> t.user.name,
+        "text" => t.text,
+        "favorite_count" => t.favorite_count,
+        "retweet_count" => t.retweet_count,
+        "media" => t.entities.media |> Enum.map(&(extractionMedia(&1)))
+      }
+    end
+  end
+
+  def extractionMedia(m) do
+    m.media_url
+  end
+
 end
